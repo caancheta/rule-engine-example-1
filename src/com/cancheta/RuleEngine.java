6ad37646662582df1b0cffc1ecffdc6f7103020b
Chris Ancheta <ancheta.c93@gmail.com>
2022-12-16 19:46:39 -0500

    Added comments relating design to rules engine explanation

--- src/com/cancheta/RuleEngine.java
+++ src/com/cancheta/RuleEngine.java
@@ -6,13 +6,15 @@ import com.cancheta.Models.Phone;
 import com.cancheta.Rules.IRule;
 
 public class RuleEngine {
+    // Persisted rules set. In this example, this is an in-mem rules set.
     List<IRule<Phone,Phone>> rulesList = new ArrayList<>();
 
     public Phone rule (Phone phone) {
+        // Inference Engine
         return rulesList.stream()
-            .filter(rule -> rule.matches(phone))
-            .map(rule -> rule.process(phone))
-            .findFirst()
+            .filter(rule -> rule.matches(phone)) // MATCH the 'fact' / data to the appropriate rule
+            .map(rule -> rule.process(phone)) // EXECUTE the process that MATCH the rule
+            .findFirst() // RESOLVE conflicts (i.e. 'facts' that match multiple rules
             .orElseThrow(() -> new RuntimeException("No matching rule found"));
     }
 
